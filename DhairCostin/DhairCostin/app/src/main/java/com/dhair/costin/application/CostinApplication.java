de090ceba4957b50f0db0238400ab3cd5783f1c2
dengshengjin <dengshengjin0804@qq.com>
2016-01-12 20:07:06 +0800

    add Hotfix

--- DhairCostin/DhairCostin/app/src/main/java/com/dhair/costin/application/CostinApplication.java
+++ DhairCostin/DhairCostin/app/src/main/java/com/dhair/costin/application/CostinApplication.java
@@ -1,10 +1,14 @@
 package com.dhair.costin.application;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.dhair.common.process.ProcessManager;
 import com.dhair.costin.BuildConfig;
 import com.dhair.hotfix.DhairHotFix;
 import com.dhair.uninstall.watcher.Watcher;
+import com.orhanobut.logger.Logger;
+
 
 /**
  * Creator: dengshengjin on 16/1/8 20:02
@@ -14,13 +18,19 @@ public class CostinApplication extends Application {
     private static final String URL = "http://www.baidu.com";
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        if (BuildConfig.DEBUG) {
-        }
-
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
         DhairHotFix.init(this);
         DhairHotFix.loadPatch(this, "/mnt/sdcard/dhair_hotfix_patch.jar");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.init();
+        if (BuildConfig.DEBUG) {
+            Logger.e("isMyProcessInTheForeground "+ ProcessManager.isMyProcessInTheForeground());
+        }
 
         Watcher.run(this, URL, true);
 
